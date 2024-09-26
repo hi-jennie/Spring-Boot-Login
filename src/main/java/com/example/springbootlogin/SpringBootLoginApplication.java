47490a9c0bb06efcfe889ab297330e6e0980ec15
hi-jennie <jennie.wang2024@gmail.com>
2024-09-26 21:57:41 +0800

    create a class

--- src/main/java/com/example/springbootlogin/SpringBootLoginApplication.java
+++ src/main/java/com/example/springbootlogin/SpringBootLoginApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLoginApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLoginApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoginApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/springbootlogin/SpringBootLoginApplicationTests.java
+++ src/test/java/com/example/springbootlogin/SpringBootLoginApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootLoginApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
