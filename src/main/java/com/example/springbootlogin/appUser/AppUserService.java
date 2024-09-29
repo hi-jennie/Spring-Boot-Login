9dd3934fcb09032a64d858c895c24ae3085eb86d
hi-jennie <jennie.wang2024@gmail.com>
2024-09-29 22:14:01 +0800

    add ConfirmationToken

--- src/main/java/com/example/springbootlogin/appUser/AppUserService.java
+++ src/main/java/com/example/springbootlogin/appUser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.springbootlogin.appUser;
 
+import com.example.springbootlogin.registration.token.ConfirmationToken;
+import com.example.springbootlogin.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 // this class is specific for spring security
 // how to find users once we try to log in
@@ -18,6 +23,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
     @Override
     // loadUserByUsername 方法：这个方法在用户登录时被调用，它接收一个邮箱（或用户名）作为参数，去数据库中查找用户。
     // 如果找到了用户，就返回用户的详细信息。如果没找到，就抛出异常，表示用户不存在。
@@ -36,14 +42,23 @@ public class AppUserService implements UserDetailsService {
         if(userExists){
             throw new IllegalStateException("email already taken");
         }
-
+        // encode the password and save it to the database
         String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
         appUser.setPassword(encodedPassword);
-
         appUserRepository.save(appUser);
+        // create a token
+        // the token is used to confirm if the email is valid
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
 
-        // TODO: Send confirmation token
+        // TODO: send email
 
-        return "it works";
+        return token;
     }
 }
---
+++ src/main/java/com/example/springbootlogin/registration/token/ConfirmationToken.java
@@ -0,0 +1,52 @@
+package com.example.springbootlogin.registration.token;
+
+import com.example.springbootlogin.appUser.AppUser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+    @Id
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence")
+    private Long id;
+    @Column(nullable = false)
+    private String token;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+
+    // many ConfirmationToken can be associated with one AppUser
+    @ManyToOne
+    //  foreign key: used to define the relationship between two entities
+    @JoinColumn(
+            nullable = false,
+            name = "app_user_id")
+    private AppUser appUser;
+
+    public ConfirmationToken(String token,
+                             LocalDateTime createdAt,
+                             LocalDateTime expiresAt,
+                             AppUser appUser) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.appUser = appUser;
+    }
+
+}
